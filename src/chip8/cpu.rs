1c78c832373b9ffba329d5761ad6104f5282922e
DLMartin- <ashinyacorn@gmail.com>
2022-04-12 19:32:04 -0400

    Refactor

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -1,76 +1,31 @@
-use super::memory::{read_from, write_into, Memory};
-use byteorder::{BigEndian, ByteOrder, LittleEndian};
-
+const FOUR_KB: usize = 4096;
+const REGISTER_COUNT: usize = 16;
+const STACK_SIZE: usize = 16;
+const DISPLAY_SIZE: usize = 64 * 32;
 pub struct Cpu {
-    memory: Memory,
-    display: [u8; 64 * 32],
+    memory: [u8; FOUR_KB],
+    stack: [u16; STACK_SIZE],
+    display: [u8; DISPLAY_SIZE],
+    registers: [u8; REGISTER_COUNT],
+    index: u16,
     program_counter: u16,
-    index_register: u16,
-    stack: Vec<u16>,
+    stack_pointer: u8,
     delay_timer: u8,
     sound_timer: u8,
-    v0: u8,
-    v1: u8,
-    v2: u8,
-    v3: u8,
-    v4: u8,
-    v5: u8,
-    v6: u8,
-    v7: u8,
-    v8: u8,
-    v9: u8,
-    va: u8,
-    vb: u8,
-    vc: u8,
-    vd: u8,
-    ve: u8,
-    vf: u8,
 }
 
 impl Cpu {
     pub fn new() -> Self {
-        Self {
-            memory: Memory::new(),
-            display: [0; 64 * 32],
+        Cpu {
+            memory: [0; FOUR_KB],
+            stack: [0; STACK_SIZE],
+            display: [0; DISPLAY_SIZE],
+            registers: [0; REGISTER_COUNT],
+            index: 0,
             program_counter: 0,
-            index_register: 0,
-            stack: Vec::new(),
+            stack_pointer: 0,
             delay_timer: 0,
             sound_timer: 0,
-            v0: 0,
-            v1: 0,
-            v2: 0,
-            v3: 0,
-            v4: 0,
-            v5: 0,
-            v6: 0,
-            v7: 0,
-            v8: 0,
-            v9: 0,
-            va: 0,
-            vb: 0,
-            vc: 0,
-            vd: 0,
-            ve: 0,
-            vf: 0,
         }
     }
-
-    pub fn write_memory(&mut self, data: &[u8], location: u16) {
-        write_into(&data, &mut self.memory, location);
-    }
-
-    pub fn read_memory(&self, location: u16) -> u8 {
-        read_from(&self.memory, location)
-    }
-}
-
-pub fn fetch(cpu: &mut Cpu) -> u16 {
-    let begin = cpu.program_counter as usize;
-    let end = begin + 2;
-    let buf = &cpu.memory[begin..end];
-
-    cpu.program_counter += 2;
-
-    BigEndian::read_u16(buf)
 }
