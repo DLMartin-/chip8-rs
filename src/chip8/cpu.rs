bfc1ddc2ba20bd8593896bb08e4551b4ace57c44
DLMartin- <ashinyacorn@gmail.com>
2022-04-13 17:04:48 -0400

    ..

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -91,7 +91,11 @@ impl Cpu {
                 // i.e. toggle the pixel
                 // 0x80 = 1000 0000 : allows to check each pixel in the sprite
                 if (sprite[j as usize] & (0x80 >> i)) != 0x00 {
-                    self.display[y as usize][x as usize] ^= 0xAA;
+                    if (self.display[y as usize][x as usize] != 0) {
+                        self.display[y as usize][x as usize] = 0;
+                    } else {
+                        self.display[y as usize][x as usize] = 0xAA;
+                    }
                 }
             }
         }
